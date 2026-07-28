29f6632fc283bd7d3eb78681e84547e76b2874a8
Agustín Rosas <rosasalvaagustin@gmail.com>
2026-07-27 22:34:19 -0600

    refactor: separar la lógica en funciones independientes

--- main.py
+++ main.py
@@ -7,24 +7,34 @@ load_dotenv()
 api_key = os.getenv("EXCHANGE_API_KEY")
 
 # Realiza la petición a la API y convierte la respuesta a formato JSON
-try:
-    respuesta = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD", timeout=10)
-    datos_api = respuesta.json()
+def obtener_datos_api():
+    try:
+        respuesta = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD", timeout=10)
+        return respuesta.json()
+    except requests.exceptions.ConnectionError:
+        print("Error: No se pudo conectar al servidor. Revisa tu red")
+    except requests.exceptions.Timeout:
+        print("Error: El servidor tardo demasiado en responder")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
+# Extrae los tasa de conversión
+def extraer_tasa(codigo_seleccionado):
+    datos_api = obtener_datos_api()
+    tasa_conversion = datos_api["conversion_rates"][codigo_seleccionado]
+    return round(tasa_conversion, 2)
+
+# Orquesta el input y muestra el resultado final
+def main():
     # Solicita al usuario el código de la moneda
     print("Escribe el código de tres letras de la moneda (por ejemplo: EUR, JPY o GBP) para conocer su equivalencia respecto al dólar estadounidense (USD).")
-    codigo_seleccionado = input("Escribe el código de la moneda: ").upper()
-
     try:
-        tasa_conversion = datos_api["conversion_rates"][codigo_seleccionado]
-        tasa_redondeada = round(tasa_conversion, 2)
-        print(f"1 USD = {tasa_redondeada} {codigo_seleccionado}")
+        codigo_seleccionado = input("Escribe el código de la moneda: ").upper()
+        resultado_final = extraer_tasa(codigo_seleccionado)
+        print(f"1 USD = {resultado_final} {codigo_seleccionado}")
     except KeyError:
         print("El código de moneda ingresado no existe.")
 
-except requests.exceptions.ConnectionError:
-    print("Error: No se pudo conectar al servidor. Revisa tu red")
-except requests.exceptions.Timeout:
-    print("Error: El servidor tardo demasiado en responder")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+# Ejecuta solo si se esta en este archivo
+if __name__ == "__main__":
+    main()
